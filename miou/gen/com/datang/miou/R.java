9f966bf2dbe8bb4a64166b023a62adcc814482bf
unknown <xuxuhui@sl2-42670-b01.BJ.DATANGMOBILE.com>
2015-03-03 17:02:12 +0800

    first commit

--- miou/gen/com/datang/miou/R.java
+++ miou/gen/com/datang/miou/R.java
@@ -8,6 +8,9 @@
 package com.datang.miou;
 
 public final class R {
+    public static final class array {
+        public static final int testtype=0x7f070000;
+    }
     public static final class attr {
     }
     public static final class color {
@@ -20,7 +23,7 @@ public final class R {
         /**  按钮绿 
          */
         public static final int button_green=0x7f060007;
-        public static final int custom_theme_color=0x7f060008;
+        public static final int custom_theme_color=0x7f060009;
         /**  Dialog标题蓝 
          */
         public static final int dialog_title_blue=0x7f060003;
@@ -30,6 +33,7 @@ public final class R {
         /**  菜单默认底色 
          */
         public static final int menu_gray=0x7f060005;
+        public static final int table_background=0x7f060008;
         /**  标题蓝 
          */
         public static final int title_blue=0x7f060004;
@@ -52,43 +56,148 @@ public final class R {
         public static final int start_bg=0x7f02000b;
     }
     public static final class id {
-        public static final int app_title_left=0x7f070013;
-        public static final int app_title_right_txt=0x7f070015;
-        public static final int app_title_value=0x7f070014;
-        public static final int bmap_view=0x7f07000e;
-        public static final int gen_map_title=0x7f070003;
-        public static final int gen_map_view=0x7f070004;
-        public static final int gen_realtabcontent=0x7f070000;
-        public static final int gen_tabcontent=0x7f070002;
-        public static final int gen_tabhost=0x7f070001;
-        public static final int main_bottom_layout=0x7f070006;
-        public static final int main_btn_cus=0x7f07000d;
-        public static final int main_btn_datas=0x7f070008;
-        public static final int main_btn_exit=0x7f070009;
-        public static final int main_btn_gen=0x7f07000a;
-        public static final int main_btn_inner=0x7f07000c;
-        public static final int main_btn_setting=0x7f070007;
-        public static final int main_btn_ssv=0x7f07000b;
-        public static final int main_image_top=0x7f070005;
-        public static final int start_btn_cancel=0x7f070012;
-        public static final int start_btn_ok=0x7f070011;
-        public static final int start_txt_email=0x7f070010;
-        public static final int start_txt_imei=0x7f07000f;
+        public static final int add=0x7f08002d;
+        public static final int add_testing_plan_button=0x7f080040;
+        public static final int app_title_left=0x7f080055;
+        public static final int app_title_right_txt=0x7f080057;
+        public static final int app_title_value=0x7f080056;
+        public static final int bmap_view=0x7f08003e;
+        public static final int box1=0x7f08000a;
+        public static final int box2=0x7f08000b;
+        public static final int box3=0x7f08000c;
+        public static final int cell_info_layout=0x7f08000d;
+        public static final int cell_info_textView=0x7f08000e;
+        public static final int chart=0x7f08002c;
+        public static final int check_box_group=0x7f080009;
+        public static final int decrease_button=0x7f080054;
+        public static final int delete_testing_plan_button=0x7f080041;
+        public static final int detail_textView=0x7f080043;
+        public static final int event_listView=0x7f080025;
+        public static final int event_textView=0x7f080021;
+        public static final int exception_textView=0x7f080033;
+        public static final int gen_map_log_toggleButton=0x7f080002;
+        public static final int gen_map_review_button=0x7f080004;
+        public static final int gen_map_start_button=0x7f080003;
+        public static final int gen_map_title=0x7f080001;
+        public static final int gen_map_view=0x7f080008;
+        public static final int gen_realtabcontent=0x7f080005;
+        public static final int gen_tabcontent=0x7f080007;
+        public static final int gen_tabhost=0x7f080006;
+        public static final int gsm_data_info_table=0x7f08001a;
+        public static final int head_layout=0x7f08002e;
+        public static final int id_textView=0x7f080051;
+        public static final int idle_info_table=0x7f08001b;
+        public static final int increase_button=0x7f080052;
+        public static final int index_textView=0x7f08002b;
+        public static final int indicator_1=0x7f080013;
+        public static final int indicator_2=0x7f080014;
+        public static final int indicator_3=0x7f080015;
+        public static final int indicator_4=0x7f080016;
+        public static final int indicator_5=0x7f080017;
+        public static final int indicator_6=0x7f080018;
+        public static final int indicator_7=0x7f080019;
+        public static final int label_editText=0x7f080000;
+        public static final int log_picker_listView=0x7f080034;
+        public static final int lte_data_info_table=0x7f08001c;
+        public static final int main_bottom_layout=0x7f080036;
+        public static final int main_btn_cus=0x7f08003d;
+        public static final int main_btn_datas=0x7f080038;
+        public static final int main_btn_exit=0x7f080039;
+        public static final int main_btn_gen=0x7f08003a;
+        public static final int main_btn_inner=0x7f08003c;
+        public static final int main_btn_setting=0x7f080037;
+        public static final int main_btn_ssv=0x7f08003b;
+        public static final int main_image_top=0x7f080035;
+        public static final int name1_textView=0x7f080048;
+        public static final int name2_textView=0x7f08004a;
+        public static final int name_textView=0x7f08002f;
+        public static final int neighbor_cell_info_table=0x7f080010;
+        public static final int path_textView=0x7f080030;
+        public static final int scripts_spinner=0x7f08003f;
+        public static final int search_event_ImageButton=0x7f080023;
+        public static final int search_event_editText=0x7f080024;
+        public static final int search_event_layout=0x7f080022;
+        public static final int search_signal_ImageButton=0x7f080028;
+        public static final int search_signal_editText=0x7f080029;
+        public static final int search_signal_layout=0x7f080027;
+        public static final int select_checkBox=0x7f080050;
+        public static final int serving_cell_info_table=0x7f08000f;
+        public static final int signal_listView=0x7f08002a;
+        public static final int signal_textView=0x7f080026;
+        public static final int size_textView=0x7f080031;
+        public static final int start_btn_cancel=0x7f080047;
+        public static final int start_btn_ok=0x7f080046;
+        public static final int start_txt_email=0x7f080045;
+        public static final int start_txt_imei=0x7f080044;
+        public static final int td_data_info_table=0x7f08001d;
+        public static final int test_plan_listView=0x7f080042;
+        public static final int time_textView=0x7f080032;
+        public static final int times_textView=0x7f080053;
+        public static final int value1_textView=0x7f080049;
+        public static final int value2_textView=0x7f08004b;
+        public static final int value3_textView=0x7f08004c;
+        public static final int value4_textView=0x7f08004d;
+        public static final int value5_textView=0x7f08004e;
+        public static final int value6_textView=0x7f08004f;
+        public static final int viewGroup=0x7f080012;
+        public static final int viewPager=0x7f080011;
+        public static final int vogsm_info_table=0x7f08001e;
+        public static final int volte_info_table=0x7f08001f;
+        public static final int votd_info_table=0x7f080020;
     }
     public static final class layout {
-        public static final int gen=0x7f030000;
-        public static final int gen_map=0x7f030001;
-        public static final int gen_menu_item=0x7f030002;
-        public static final int gen_params=0x7f030003;
-        public static final int gen_signal=0x7f030004;
-        public static final int gen_tel_stat=0x7f030005;
-        public static final int main=0x7f030006;
-        public static final int map=0x7f030007;
-        public static final int start=0x7f030008;
-        public static final int title=0x7f030009;
+        public static final int dialog_label_info=0x7f030000;
+        public static final int gen=0x7f030001;
+        public static final int gen_map=0x7f030002;
+        public static final int gen_menu_item=0x7f030003;
+        public static final int gen_params=0x7f030004;
+        public static final int gen_params_gsm_data=0x7f030005;
+        public static final int gen_params_idle_triple=0x7f030006;
+        public static final int gen_params_lte_data=0x7f030007;
+        public static final int gen_params_td_data=0x7f030008;
+        public static final int gen_params_vogsm=0x7f030009;
+        public static final int gen_params_volte=0x7f03000a;
+        public static final int gen_params_votd=0x7f03000b;
+        public static final int gen_signal=0x7f03000c;
+        public static final int gen_tel_stat=0x7f03000d;
+        public static final int gen_tel_stat_data_connection=0x7f03000e;
+        public static final int gen_tel_stat_data_quality=0x7f03000f;
+        public static final int gen_tel_stat_message=0x7f030010;
+        public static final int gen_tel_stat_voice=0x7f030011;
+        public static final int log_picker_dialog=0x7f030012;
+        public static final int main=0x7f030013;
+        public static final int map=0x7f030014;
+        public static final int script_setting=0x7f030015;
+        public static final int signal_detail=0x7f030016;
+        public static final int start=0x7f030017;
+        public static final int table_row_4_items_blue=0x7f030018;
+        public static final int table_row_4_items_white=0x7f030019;
+        public static final int table_row_7_items_blue=0x7f03001a;
+        public static final int table_row_7_items_white=0x7f03001b;
+        public static final int test_log_list_item=0x7f03001c;
+        public static final int test_plan_list_item=0x7f03001d;
+        public static final int title=0x7f03001e;
     }
     public static final class string {
+        public static final int add_testing_plan=0x7f04000b;
         public static final int app_name=0x7f040000;
+        public static final int cell_info=0x7f040011;
+        public static final int decrease=0x7f040010;
+        public static final int delete_testing_plan=0x7f04000c;
+        public static final int dialog_label_info_title=0x7f040009;
+        public static final int dialog_log_picker=0x7f04000a;
+        public static final int gen_map_is_logging=0x7f040004;
+        public static final int gen_map_review=0x7f040003;
+        public static final int gen_map_signal_detail_title=0x7f040006;
+        public static final int gen_map_start=0x7f040001;
+        public static final int gen_map_stop=0x7f040002;
+        public static final int gen_map_title=0x7f040005;
+        public static final int gen_map_title_export_button=0x7f040008;
+        public static final int gen_map_title_script_button=0x7f040007;
+        public static final int gen_script_save_button=0x7f04000e;
+        public static final int gen_script_title=0x7f04000d;
+        public static final int increase=0x7f04000f;
+        public static final int params=0x7f040012;
     }
     public static final class style {
         /** 
--- miou/src/com/datang/miou/views/gen/GenActivity.java
+++ miou/src/com/datang/miou/views/gen/GenActivity.java
@@ -1,14 +1,28 @@
 package com.datang.miou.views.gen;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTabHost;
+import android.support.v4.app.NavUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TabWidget;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import com.datang.miou.datastructure.Chart;
+import com.datang.miou.datastructure.Chart.Point;
+import com.datang.miou.datastructure.TestLog;
+import com.datang.miou.views.dialogs.LogPickerDialogFragment;
+import com.datang.miou.views.gen.GenTelStatFragment;
 
 import com.datang.miou.R;
 
@@ -17,7 +31,9 @@ import com.datang.miou.R;
  * 
  * @author suntongwei
  */
-public class GenActivity extends FragmentActivity {
+public class GenActivity extends FragmentActivity implements LogPickerDialogFragment.Callbacks{
+
+	private static final String DIALOG_LOG_PICKER = "log_picker";
 
 	//定义FragmentTabHost对象  
     public FragmentTabHost mTabHost;  
@@ -31,7 +47,25 @@ public class GenActivity extends FragmentActivity {
       
     //Tab选项卡的文字  
     private String mTextviewArray[] = {"地图", "指标", "信令", "话务统计"};
+
+	private TextView mTitleTextView;
+
+	private TextView mScriptButton;
+
+	private ImageView mBackButton;
+
+	private Button mStartButton;
+
+	private Button mReviewButton;
+
+	private ToggleButton mLogButton;
+
+	protected boolean isTesting;
+
+	protected boolean isLogging;
     
+	private TestLog mSelectedTestLog;
+
     public void onCreate(Bundle savedInstanceState) {  
         super.onCreate(savedInstanceState);  
         setContentView(R.layout.gen);  
@@ -71,8 +105,90 @@ public class GenActivity extends FragmentActivity {
 				}
 			}
 		});
+        
+        mTitleTextView = (TextView) findViewById(R.id.app_title_value);
+		mTitleTextView.setText(R.string.gen_map_title);
+		
+		mScriptButton = (TextView) findViewById(R.id.app_title_right_txt);
+		mScriptButton.setText(R.string.gen_map_title_script_button);
+		mScriptButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				// TODO 自动生成的方法存根
+				Intent intent = new Intent(getApplicationContext(), ScriptSettingActivity.class);
+				startActivity(intent);
+			}
+		});
+		
+		mBackButton = (ImageView) findViewById(R.id.app_title_left);
+		mBackButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				// TODO 自动生成的方法存根
+				try {
+					if (NavUtils.getParentActivityName((Activity) getApplicationContext()) != null) {
+						NavUtils.navigateUpFromSameTask((Activity) getApplicationContext());
+					}
+				} catch (Exception e) {
+					finish();
+				}		
+			}
+		});
+		
+		mStartButton = (Button) findViewById(R.id.gen_map_start_button);
+		mStartButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (isTesting) {
+					mStartButton.setText(R.string.gen_map_start);
+					mReviewButton.setEnabled(true);
+					isTesting = false;
+					stopTesting();
+				} else {
+					mStartButton.setText(R.string.gen_map_stop);
+					mReviewButton.setEnabled(false);
+					isTesting = true;
+					startTesting();
+				}
+			}
+		});
+		
+		mReviewButton = (Button) findViewById(R.id.gen_map_review_button);
+		mReviewButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				reviewTesting();
+			}
+		});
+		
+		mLogButton = (ToggleButton) findViewById(R.id.gen_map_log_toggleButton);
+		mLogButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				isLogging = mLogButton.isChecked();
+			}
+		});
     }  
-                  
+          
+	private void reviewTesting() {
+		// TODO 自动生成的方法存根
+		FragmentManager fm = getSupportFragmentManager();
+		LogPickerDialogFragment dialog = LogPickerDialogFragment.newInstance(this);
+		dialog.show(fm, DIALOG_LOG_PICKER);
+	}
+
+	private void stopTesting() {
+		// TODO 自动生成的方法存根
+		
+	}
+
+	private void startTesting() {
+		// TODO 自动生成的方法存根
+		
+	}
+	
     /** 
      * 给Tab按钮设置图标和文字 
      */  
@@ -84,5 +200,11 @@ public class GenActivity extends FragmentActivity {
         	view.setTextColor(getResources().getColor(R.color.white));
         }
         return view;  
-    }  
+    }
+
+	@Override
+	public void refreshActivity(TestLog log) {
+		// TODO 自动生成的方法存根
+		mSelectedTestLog = log;
+	}
 }
